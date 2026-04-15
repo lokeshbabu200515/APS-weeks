d233f0ad7a4a2f6844880b65e4e453dd5fd3bc17
Lokesh Babu <lokeshbabunettem2@gmail.com>
2026-04-15 11:29:27 +0530

    Time: 7 ms (60.3%), Space: 47.1 MB (67.09%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        int[] indegree = new int[numCourses];
+
+        for (int[] p : prerequisites) {
+            graph.get(p[1]).add(p[0]);
+            indegree[p[0]]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        int count = 0;
+
+        while (!queue.isEmpty()) {
+            int course = queue.poll();
+            count++;
+
+            for (int next : graph.get(course)) {
+                if (--indegree[next] == 0) {
+                    queue.add(next);
+                }
+            }
+        }
+
+        return count == numCourses;
+    }
+}
